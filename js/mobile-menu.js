9b3dec98b75bb7efd2509f8627a2a940e04cf939
VitaliiDeKo <108880791+VitaliiDeKo@users.noreply.github.com>
2022-09-20 00:17:21 +0300

    hw-8
    first try

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,12 +3,21 @@
     openMenuBtn: document.querySelector("[data-menu-open]"),
     closeMenuBtn: document.querySelector("[data-menu-close]"),
     menu: document.querySelector("[data-menu]"),
+    body: document.querySelector("body"),
+    menulist: document.querySelector(".mobile-menu__list-nav"),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
   refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.menulist.addEventListener("click", removeMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
+  }
+
+  function removeMenu() {
+    refs.menu.classList.add("is-hidden");
+    refs.body.classList.remove("no-scroll")
   }
 })();
